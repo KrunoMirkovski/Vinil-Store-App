7e75fd8266e6ab72f92a8a4f9735a56582e4d8cb
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2024-12-26 02:53:50 +0100

    defining colours and fonts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <nav>Navbar</nav>
-    <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6'>
+    <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6 font-primary'>
     <Outlet/>
     </main>
      <footer>Footer</footer>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#002C54",
+        secondary: "#C5001A",
+        blackBG: "#FDF6F6",
+        favorite: "#FFB51D",
+      },
+      fontFamily: {
+        primary: ["Inter", "serif"],
+        secondary: ["Lato", "serif"],
+      },
+    },
   },
   plugins: [],
 };
