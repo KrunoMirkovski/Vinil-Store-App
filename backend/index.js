a976a5fae668864bbf315dcfcec55aa93ff60600
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2025-03-25 01:30:48 +0100

    edit Vercel route

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://vinil-store-app.vercel.app/"],
+    origin: ["http://localhost:5173", "https://vinil-store-app.vercel.app"],
     credentials: true,
   })
 );
